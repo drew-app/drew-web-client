a1f149474873d699a34c4a987e77c7ae654f2988
Drew Arrigoni <arrigoni.andrew@gmail.com>
2018-08-12 15:01:34 -0400

    S3 deploy config

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  pluginOptions: {
+    s3Deploy: {
+      assetPath: 'dist',
+      bucket: 'drew-app-webclient',
+      region: 'us-east-2',
+      pwa: false,
+      enableCloudfront: true,
+      uploadConcurrency: 5,
+      cloudfrontId: 'E5NWXFQKDXAWE',
+      cloudfrontMatchers: '/*'
+    }
+  }
+}
